7417c90a4870582eaddb413b765364391b7a94be
Umair khan <ukkhan5476@gmail.com>
2026-04-11 07:37:06 +0530

    feat:auth routes

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
 export default app;
--- Server/src/routes/auth.route.js
+++ Server/src/routes/auth.route.js
@@ -1,4 +1,9 @@
 import { Router } from "express";
-
+import { register, login } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
+
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+
+export default authRouter;
