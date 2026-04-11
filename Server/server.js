25aeac6de96235b08008c09af2c3822d94ee4bf3
Umair khan <ukkhan5476@gmail.com>
2026-04-11 19:10:59 +0530

    feat: boiler plate of fronntend with tailwind intigration

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ import app from "./src/app.js";
 import config from "./src/config/dotenv.config.js";
 import connectDB from "./src/config/dataBase.js";
 
-app.listen(config.PORT, () => {
-  connectDB();
+app.listen(config.PORT,  () => {
+   connectDB();
   console.log("server is runing");
 });
