f06f3ec15230f6653c279cb460d11cf723415753
Umair khan <ukkhan5476@gmail.com>
2026-04-13 19:02:53 +0530

    feat:add forgotpassword routes

--- Server/src/routes/auth.route.js
+++ Server/src/routes/auth.route.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import {
+  register,
+  login,
+  logout,
+  sendOtp,
+  verifyOtp,
+  resetPassword,
+} from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.get("/logout", logout);
+authRouter.post("/send-otp", sendOtp);
+authRouter.post("/verify-otp", verifyOtp);
+authRouter.post("/reset-password", resetPassword);
 
 export default authRouter;
